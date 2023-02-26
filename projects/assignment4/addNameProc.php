2f1721dde0b4e2ebb2f2bff3bb90d7ea649ffc4c
bpetroski <brenden.petroski@gmail.com>
2023-02-26 09:24:46 -0500

    still figuring out php form input

--- projects/assignment4/addNameProc.php
+++ projects/assignment4/addNameProc.php
@@ -3,6 +3,20 @@
     class addNameProc{
         private $firstName;
         private $lastName;
+        private $nameList;
+
+        // function __construct(){
+        //     echo fuck
+        // }
+
+        function AddNamesProc(){
+            $firstName = {$_POST["name-input"]};
+        }
+
+        function addClearNames(){
+
+        }
+
     }
 
 
