05ca12d726b10bc22129dd43ead1611f728864de
bpetroski <brenden.petroski@gmail.com>
2023-03-10 18:09:14 -0500

    git is working!!

--- projects/assignment5/index.php
+++ projects/assignment5/index.php
@@ -16,7 +16,7 @@
         <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha1/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-GLhlTQ8iRABdZLl6O3oVMWSktQOp6b7In1Zl3/Jr59b6EGGoI1aFkw7cmDA6j6gD" crossorigin="anonymous">
     </head>
     <body>
-        <p><?php echo "bjp3 git working" ?></p>
+        <p><?php echo "bjp is git finally working?" ?></p>
         <div class="container">
             <h1>File and Directory Assignment</h1>
             <p>Enter a folder name and the contents of a file. Folder names should contain alpha numeric characters only.</p>
