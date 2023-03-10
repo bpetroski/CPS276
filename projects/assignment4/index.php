f4fdeb35abb20e427dda1b522292e67e4dcfa823
bpetroski <brenden.petroski@gmail.com>
2023-03-09 23:13:18 -0500

    perms denied

--- projects/assignment4/index.php
+++ projects/assignment4/index.php
@@ -1,13 +1,14 @@
 <?php
 
-    $myname = "Brenden Petroski";
-    $testclass = new addNamesProc();
+    // require_once 'addNamesProc.php';
+    // $myname = "Brenden Petroski";
+    // $testclass = new addNamesProc();
+    
+    // $myname = $testclass->formatName($myname);
+
     $output = "";
-
-    require_once 'addNameProc.php';
-    $testclass->formatName($myname);
-
     if(count($_POST) > 0){
+        echo "fuck shit up";
         require_once 'addNameProc.php';
         $addName = new AddNamesProc();
         $output = $addName->addClearNames();
@@ -24,17 +25,17 @@
         <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha1/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-GLhlTQ8iRABdZLl6O3oVMWSktQOp6b7In1Zl3/Jr59b6EGGoI1aFkw7cmDA6j6gD" crossorigin="anonymous">
     </head>
     <body>
-        <p><?php echo $myname ?></p>
+        <p><?php echo "bjp" ?></p>
         <div class="container">
             <h1>Add Names</h1>
-            <form action="addNameProc.php" method="post">
+            <form name="Names" action="addNamesProc.php" method="post">
                 <div class="form-group">
-                    <button type="button" class="btn btn-primary" name="Add-Names" id="add-name-btn">Add Name</button>
+                    <input type="button" class="btn btn-primary" name="Add-Names" id="add-name-btn" value="Add Names">
                     <button type="button" class="btn btn-primary" name="Clear-Names" id="clear-names-btn">Clear Names</button>
                 </div>
                 <div class="form-group">
                     <label for="name-input">Enter Name:</label>
-                    <input type="text" class="form-control" id="name-input">
+                    <input type="text" class="form-control" name="name-input" id="name-input" value="Brenden Petroski">
                 </div>
                 <div class="form-group">
                     <label for="namelist">List of Names:</label>
