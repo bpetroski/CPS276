f535b8117abb0033321559e50d985b0b6bdde854
bpetroski <brenden.petroski@gmail.com>
2023-03-10 11:36:32 -0500

    assignment5 init

--- projects/assignment5/index.php
+++ projects/assignment5/index.php
@@ -1,8 +1,41 @@
 <?php
 
-mkdir("test", 0777);
-
+// mkdir("test", 0777);
+// touch("test");
 
 
 
 ?>
+
+<!doctype html>
+<html lang="en">
+    <head>
+        <meta charset="utf-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1">
+        <title>Directory Creator</title>
+        <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha1/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-GLhlTQ8iRABdZLl6O3oVMWSktQOp6b7In1Zl3/Jr59b6EGGoI1aFkw7cmDA6j6gD" crossorigin="anonymous">
+    </head>
+    <body>
+        <p><?php echo "bjp" ?></p>
+        <div class="container">
+            <h1>File and Directory Assignment</h1>
+            <p>Enter a folder name and the contents of a file. Folder names should contain alpha numeric characters only.</p>
+            <form name="Directories" action="Directories.php" method="post">
+                <div class="form-group">
+                    <label for="name-input">Folder Name:</label>
+                    <input type="text" class="form-control" name="name-input" id="name-input" value="TestFolder123">
+                </div>
+                <br>
+                <div class="form-group">
+                    <label for="text-input">File Content:</label>
+                    <textarea style="height: 500px;" class="form-control"id="text-input" name="text-input">This is where you enter text</textarea>
+                </div>
+                <br>
+                <div>
+                    <button type="submit" class="btn btn-primary" name="submitBtn" id="submitBtn">Submit</button>
+                </div>
+            </form>
+        </div>
+        
+    </body>
+</html> 
