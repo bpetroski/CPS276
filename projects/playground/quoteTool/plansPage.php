28f1c625afb91fc8edbcce49ef391b7d40c5faab
bpetroski <brenden.petroski@gmail.com>
2023-03-13 16:50:51 -0400

    personal project

---
+++ projects/playground/quoteTool/plansPage.php
@@ -0,0 +1,42 @@
+<?php
+
+    $output = "<br>";
+
+?>
+
+<!doctype html>
+<html lang="en">
+    <head>
+        <meta charset="utf-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1">
+        <title>Voice Plans</title>
+        <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha1/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-GLhlTQ8iRABdZLl6O3oVMWSktQOp6b7In1Zl3/Jr59b6EGGoI1aFkw7cmDA6j6gD" crossorigin="anonymous">
+        <link rel="stylesheet" type="text/css" href="navHeader.css">
+    
+    </head>
+    <body>
+        <header style="background-color: #333; color: #fff;">
+            <div class="container">
+                <nav class="nav-header">
+                    <ul>
+                        <p style="padding-right: 75px;"></p>
+                        <li><a href="plansPage.php">Voice Plans</a></li>
+                        <li><a href="phonesPage.php">Phones</a></li>
+                        <li><a href="#">Data Plans</a></li>
+                        <li><a href="#">Cart</a></li>
+                    </ul>
+                </nav>
+                <div class="logo-container"> 
+                    <div style="padding-right: 15px;">
+                        <a href="https://russet-v8.wccnet.edu/~bjpetroski/projects/playground" target="_self"> <img src="src/tmo-logo-v4.svg" alt="T-Mobile"></a> 
+                    </div> 
+                    <div class="header-text">
+                        <h1>Voice Plans Page</h1>
+                        <p>Web App by Brenden Petroski</p>
+                    </div>
+                </div>
+            </div>
+        </header>
+        
+    </body>
+</html> 
