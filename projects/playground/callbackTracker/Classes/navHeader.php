9c12f40d279c133186febecbecde72e885a6723d
bpetroski <brenden.petroski@gmail.com>
2023-04-05 15:27:52 -0400

    quick n dirty way to hide nav on login

--- projects/playground/callbackTracker/Classes/navHeader.php
+++ projects/playground/callbackTracker/Classes/navHeader.php
@@ -3,10 +3,10 @@
 class navHeader {
 
     public function nav($header){
-        $nav = <<<NAV
-
-        <header style="background-color: #333; color: #fff;">
-            <div class="container">
+        if($header == 'Callback Log In'){
+            $navBar = "";
+        }else{
+            $navBar = '
                 <nav class="nav-header">
                     <ul>
                         <li><a href="newCallback.php">New Callback</a></li>
@@ -15,6 +15,13 @@ class navHeader {
                         <li><a href="logout.php">Logout</a></li>
                     </ul>
                 </nav>
+            ';
+        }
+        $nav = <<<NAV
+
+        <header style="background-color: #333; color: #fff;">
+            <div class="container">
+                <div>$navBar</div>
                 <div class="logo-container"> 
                     <div style="padding-right: 15px;">
                         <a href="https://russet-v8.wccnet.edu/~bjpetroski/projects/playground" target="_self"> <img src="../src/tmo-logo-v4.svg" alt="T-Mobile"></a> 
