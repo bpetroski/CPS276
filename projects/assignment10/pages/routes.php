5242469eb5c707584fb64735a8bc386fc2f56880
bpetroski <brenden.petroski@gmail.com>
2023-04-11 16:56:07 -0400

    sql queries

--- projects/assignment10/pages/routes.php
+++ projects/assignment10/pages/routes.php
@@ -2,13 +2,47 @@
 
 $path = "index.php?page=welcome";
 
+$css=<<<css
+<style>
+.nav-header {
+	display: flex;
+	padding: 10px;
+    list-style: none;
+}
+
+.nav-header ul {
+	display: flex;
+	margin: 0;
+	padding: 0;
+}
+
+.nav-header li {
+	margin-right: 20px;
+}
+</style>
+css;
 
 $nav=<<<HTML
+    $css
     <nav>
-        <ul>
-            <li><a href="index.php?page=welcome">Welcome</a></li>
-            <li><a href="index.php?page=addContact">Add Contact Information</a></li>
-            <li><a href="index.php?page=deleteContacts">Delete contact(s)</a></li>
+        <ul class="nav-header">
+            <li><a href="index.php?page=addContact">Add Contact</a></li>
+            <li><a href="index.php?page=deleteContacts">Delete contact(s)</a></li>  
+            <li><a href="logout.php">Logout</a></li>   
+        </ul>
+    </nav>
+HTML;
+
+$adminNav=<<<HTML
+    $css
+    <nav>
+        <ul class="nav-header">
+            <li><a href="index.php?page=addContact">Add Contact</a></li>
+            <li><a href="index.php?page=deleteContacts">Delete contact(s)</a></li>    
+            <li><a href="index.php?page=addAdmin">Add Admin</a></li>
+            <li><a href="index.php?page=deleteAdmin">Delete Admin(s)</a></li> 
+            <li><a href="logout.php">Logout</a></li>    
+   
         </ul>
     </nav>
 HTML;
---
+++ projects/assignment10/sql/admins.sql
@@ -0,0 +1,17 @@
+DROP TABLE IF EXISTS `admins`;
+CREATE TABLE admins (
+    admin_id    int NOT NULL AUTO_INCREMENT,
+    admin_name      char(255)   NOT NULL,
+    admin_email     char(255)   NOT NULL,
+    admin_password  char(255)   NOT NULL,
+    admin_status    char(255)   NOT NULL, -- either 'admin' or 'staff'
+    PRIMARY KEY (admin_id)
+)ENGINE=InfoID;
+
+INSERT INTO admins(admin_name, admin_email, admin_password, admin_status)
+VALUES ('Brenden Petroski', 'bjpetroski@wccnet.edu', '$2y$10$YB3QWJyvqVgP5wkLU9TOdu7DzFsvuIuaAnQ5X4yb9hFSrOVqGS2o2', 'admin');
+
+INSERT INTO admins(admin_name, admin_email, admin_password, admin_status)
+VALUES ('Scott Shaper', 'sshaper@wccnet.edu', '$2y$10$YB3QWJyvqVgP5wkLU9TOdu7DzFsvuIuaAnQ5X4yb9hFSrOVqGS2o2', 'staff');
+
+
